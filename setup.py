62975f4898872acc2d798619d3e7e0bc1bb4fddd
Birger Moell <birger.moell@gmail.com>
2022-10-07 14:55:34 +0200

    {1:-default comment}

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.106'
+VERSION = '0.0.107'
 DESCRIPTION = 'TMH Speech package'
 LONG_DESCRIPTION = 'A package for TMH Speech'
 
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     package_data={'': ['beskow/*.tsv']},
-    install_requires=['transformers', 'torch', 'torchaudio', 'speechbrain',
+    install_requires=['transformers', 'diffusers', 'torch', 'torchaudio', 'speechbrain',
                       'librosa', 'noisereduce', 'numpy', 'scipy', 'unidecode', 'inflect', 'librosa', 'python-dotenv', 'deep-phonemizer', 'argparse', 'pydub', 'pyctcdecode'],
     keywords=['python', 'speech', 'voice', 'nlp'],
     classifiers=[
